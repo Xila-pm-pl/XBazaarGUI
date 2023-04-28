b57c7296001beb599ddd4a51ce60b9e99a09a58d
Rishi <129596573+Astro-PM@users.noreply.github.com>
2023-04-28 19:44:17 +0530

    update src/Xila/Bazaar/EventListener.php

---
+++ src/Xila/Bazaar/EventListener.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace Xila\Bazaar;
+
+declare(strict_types =1);
+
+use pocketmine\event\Listener;
+use pocketmine\player\Player;
+
+class EventListener implements Listener {
+  // Todo
+}
